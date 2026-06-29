0045fcd127aa0c7eafbbfdfb3833be736cc56acc
jalnaafridi <jalnaafridi@gmail.com>
2026-06-29 11:12:03 +0200

    apps

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,4 @@
+// These files make the routes work without parentheses in file system paths
+// since we used simplified folder names (auth/ instead of (auth)/)
+
+export { default } from "@/app/auth/sign-in/page";
