624b68885b86bd80a079ec9a8b88c36c6f54d666
jalnaafridi <86564506+jalnaafridi@users.noreply.github.com>
2026-06-30 09:23:50 +0200

    Update server.ts

--- lib/server.ts
+++ lib/server.ts
@@ -3,7 +3,6 @@ import { cookies } from "next/headers";
 
 export function createServerSupabaseClient() {
   const cookieStore = cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -25,7 +24,6 @@ export function createServerSupabaseClient() {
 
 export function createServiceClient() {
   const cookieStore = cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
